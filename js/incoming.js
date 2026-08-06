79b70cd2105db25c82b57840a5571a7cb6cfb4c6
thepiratebeer <duendester@gmail.com>
2026-08-06 13:18:33 -0300

    Atualizacao automatica em 06/08/2026 as 13:18

--- js/incoming.js
+++ js/incoming.js
@@ -1,19 +1,22 @@
 const incoming = [
-/*
-{
-    nome: "Rauchbier (Defumada)",
-    ibu: "15",
-    abv: "4,6%",
-    previsao: "Carbonatação",
-    progresso: 90
-},
-*/
+
 {
     nome: "American Hop Lager",
     ibu: "27",
     abv: "4,5%",
+    previsao: "Fermentando",
+    progresso: 30
+},
+
+
+{
+    nome: "Czech Premium Pale Lager",
+    ibu: "36",
+    abv: "5,4%",
     previsao: "Brassagem",
     progresso: 10
 }
+
+
 
 ];
