4af20199eeb5eca597dd5ce2a0a8e68907a500bb
thepiratebeer <duendester@gmail.com>
2026-08-15 21:24:07 -0300

    Update cervejas.js
    Commit de jurere int

--- js/cervejas.js
+++ js/cervejas.js
@@ -13,7 +13,7 @@ const cervejas = [
     nome: "🟢 Red IPA",
     ibu: 40,
     abv: "5.5%",
-    estoque: 25,
+    estoque: 7,
     preco: 20,
     descricao: "Red IPA de corpo médio, que combina o perfil maltado de caramelo e tosta com o aroma vívido de lúpulos cítricos e resinosos. Amargor marcante e final seco, com excelente equilíbrio entre malte e lúpulo.",
 },
@@ -23,7 +23,7 @@ const cervejas = [
     ibu: 15,
     abv: "4,6%",
     preco: 20,
-    estoque: 14,
+    estoque: 13,
     descricao: "Cerveja defumada de corpo médio e alta drinkability, combinando a base limpa de malte Pilsen com notas marcantes de madeira, bacon e leve carvão provenientes do malte defumado. O amargor é moderado, equilibrando a doçura do malte sem roubar o protagonismo da fumaça. Final seco, limpo e persistente, com caráter defumado elegante que convida ao próximo gole."
 }
 /*
