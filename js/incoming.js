e1f9c10d1e800576458919b4ae46563114fb23b1
thepiratebeer <duendester@gmail.com>
2026-09-10 10:43:07 -0300

    10.09.2026(2)

--- js/incoming.js
+++ js/incoming.js
@@ -4,8 +4,8 @@ const incoming = [
     nome: "Czech PPL",
     ibu: "40",
     abv: "5,2%",
-    previsao: "Fermentando",
-    progresso: 30
+    previsao: "Produção",
+    progresso: 10
 },
 
 
@@ -13,8 +13,8 @@ const incoming = [
     nome: "WC IPA",
     ibu: "35",
     abv: "5,7%",
-    previsao: "Produção",
-    progresso: 10
+    previsao: "Fermentando",
+    progresso: 30
 },
 
 
